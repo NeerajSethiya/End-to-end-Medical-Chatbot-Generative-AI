05ac1941a27e653508a19534f1f8a87c75e2f9fa
Neeraj Sethiya <holdenmcgroin911@gmail.com>
2025-03-15 15:36:08 +0530

    folder structure added

---
+++ Setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    aurhor='Neeraj Sethiya',
+    author_email = 'nsethiya@gitam.in',
+    packages=find_packages(),
+    install_requires=[]
+)
